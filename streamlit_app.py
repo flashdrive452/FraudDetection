3ea686c60b4830521204763133187de5d50f48b3
Chukwuma Onwuka <onwuka.charles@yahoo.com>
2023-09-04 15:52:09 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,10 @@ st.title('Random Forest Classifier')
 st.markdown('## Predict Fraud or Not')
 
 # Input form with improved styling
-sepal_length = st.number_input('Enter Sepal Length', min_value=0.0, max_value=10.0, format='%f', key='sepal_length')
-sepal_width = st.number_input('Enter Sepal Width', min_value=0.0, max_value=10.0, format='%f', key='sepal_width')
-petal_length = st.number_input('Enter Petal Length', min_value=0.0, max_value=10.0, format='%f', key='petal_length')
-petal_width = st.number_input('Enter Petal Width', min_value=0.0, max_value=10.0, format='%f', key='petal_width')
+sepal_length = st.number_input('Enter Credit card Number', min_value=0.0, max_value=10.0, format='%f', key='sepal_length')
+sepal_width = st.number_input('Enter Enter transaction amount', min_value=0.0, max_value=10.0, format='%f', key='sepal_width')
+petal_length = st.number_input('Enter Enter Street', min_value=0.0, max_value=10.0, format='%f', key='petal_length')
+petal_width = st.number_input('Enter Enter City', min_value=0.0, max_value=10.0, format='%f', key='petal_width')
 
 if st.button('Predict'):
     input_data = pd.DataFrame({
@@ -45,8 +45,26 @@ if st.button('Predict'):
     #prediction_prob = model.predict_proba(input_data)
 
     st.markdown('### Prediction Results:')
-    fraud_map = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+    fraud_map = {0: 'Not Fraud', 1: 'Fraud'}
     st.markdown(f'**Predicted Fraud:**')
     st.markdown(f'**Class Probabilities:**')
     #st.write(pd.DataFrame({'Fraud or Not': ))
+
+# Input for batch prediction using CSV file
+st.markdown('## Batch Prediction (CSV Upload)')
+uploaded_file = st.file_uploader("Upload a CSV file for batch prediction", type=["csv"])
+
+if uploaded_file is not None:
+    # Read the uploaded CSV file into a DataFrame
+    batch_data = pd.read_csv(uploaded_file)
+
+    if st.button('Perform Batch Prediction'):
+        # Perform batch prediction on the uploaded data
+        #batch_predictions = model.predict(batch_data)
+        #batch_data['Predicted Species'] = batch_predictions
+
+        st.markdown('### Batch Prediction Results:')
+        st.write(batch_data)
+
+
 
